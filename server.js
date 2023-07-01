167d1ffd8eb7a1501a70757087793f042f690eec
Jairo Durango <cartuchoxl@hotmail.com>
2023-07-01 17:06:54 -0500

    autenticacion

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
 const express = require('express');
-const listViewRouter = require('./list-view-router');
-const listEditRouter = require('./list-edit-router');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const app = express();
-
 app.use(express.json());
 
+const users = [
+    { id: 1, username: 'user', password: 'pwuser' },
+    { id: 2, username: 'admin', password: 'pwadmin' }
+];
 
-const validateMethod = (req, res, next) => {
-  const validMethods = ['GET', 'POST', 'DELETE', 'PUT'];
+// Ruta /login con el método POST para autenticación
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
 
-  if (!validMethods.includes(req.method)) {
-    return res.status(405).send('Invalid HTTP method');
-  }
+    if (!user) {
+        return res.status(401).json({ error: 'Credenciales inválidas' });
+    }
 
-  next();
-};
+    // Crear un JWT con el ID del usuario
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-app.use(validateMethod);
-
-app.get('/', (req, res) => {
-  res.send("servidor express");
+    res.json({ token });
 });
 
-app.use('/tasks/view', listViewRouter);
-app.use('/tasks/edit', listEditRouter);
+// Middleware para validar el token JWT en la ruta protegida
+const authenticateToken = (req, res, next) => {
+    const token = req.headers.authorization;
 
-app.listen(8080, () => {
-  console.log('El servidor está corriendo en el puerto 8080');
+    if (!token) {
+        return res.status(401).json({ error: 'Token no proporcionado' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Token inválido' });
+        }
+
+        req.user = user;
+        next();
+    });
+};
+
+// Ruta protegida que requiere autenticación
+app.get('/protected', authenticateToken, (req, res) => {
+    res.json({ message: 'Acceso concedido a la ruta protegida' });
+});
+
+// Iniciar el servidor
+app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000');
 });
