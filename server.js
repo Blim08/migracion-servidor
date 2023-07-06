67e95fd9312f995f8050759ba3b5356b17c47cdf
Jairo Durango <cartuchoxl@hotmail.com>
2023-07-06 18:18:12 -0500

    pro-5

--- server.js
+++ server.js
@@ -1,54 +1,16 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const router = require('./router');
 
 const app = express();
+
 app.use(express.json());
 
-const users = [
-    { id: 1, username: 'user', password: 'pwuser' },
-    { id: 2, username: 'admin', password: 'pwadmin' }
-];
-
-// Ruta /login con el método POST para autenticación
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(u => u.username === username && u.password === password);
-
-    if (!user) {
-        return res.status(401).json({ error: 'Credenciales inválidas' });
-    }
-
-    // Crear un JWT con el ID del usuario
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    res.json({ token });
+app.get('/', (req, res) => {
+  res.send("express server")
 });
 
-// Middleware para validar el token JWT en la ruta protegida
-const authenticateToken = (req, res, next) => {
-    const token = req.headers.authorization;
+app.use('/api/tasks', router);
 
-    if (!token) {
-        return res.status(401).json({ error: 'Token no proporcionado' });
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({ error: 'Token inválido' });
-        }
-
-        req.user = user;
-        next();
-    });
-};
-
-// Ruta protegida que requiere autenticación
-app.get('/protected', authenticateToken, (req, res) => {
-    res.json({ message: 'Acceso concedido a la ruta protegida' });
-});
-
-// Iniciar el servidor
-app.listen(3000, () => {
-    console.log('Servidor iniciado en el puerto 3000');
+app.listen(8080, () => {
+  console.log(`Server is running on port 8080`);
 });
